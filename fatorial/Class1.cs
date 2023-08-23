d91f9e76e16f8667611f106722edd3fbea6441e5
Alisson <Alisson@DESKTOP-RKTK4TM>
2023-08-23 15:03:39 -0300

    Adicionar arquivos de projeto.

---
+++ fatorial/Class1.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fatorial
+{
+    public class Fatorial
+    {
+        public int CalculoFatorial(int num)
+        {
+            if (num == 0 || num == 1)
+                return 1;
+            else
+                return num * CalculoFatorial(num - 1);
+        }
+    }
+
+}    
+    
---
+++ fatorial/Program.cs
@@ -0,0 +1,11 @@
+﻿using fatorial;
+
+ 
+Fatorial fatorial = new Fatorial(); 
+
+for (int num = 1; num <= 10; num++)
+        {
+            int resultado = fatorial.CalculoFatorial(num); 
+            Console.WriteLine($"O fatorial de {num} é {resultado}");
+        }
+
